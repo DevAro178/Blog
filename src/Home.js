a520c42fcddb64b84af799424bc268e2b2f70210
DevAro178 <me.ammad1786@gmail.com>
2023-07-21 00:40:52 +0500

    Lec 13

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,12 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
+  const handleDelte = (id) => {
+    const newBlogs = blogs.filter((blogs) => blogs.id != id);
+    setBlogs(newBlogs);
+  };
+
   const [blogs, setBlogs] = useState([
     { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
     { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
@@ -14,12 +20,7 @@ const Home = () => {
 
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All blogs" handleDelte={handleDelte} />
     </div>
   );
 };
